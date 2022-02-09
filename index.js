eccd407de1deafea684e273511e8754702bec90c
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2022-02-08 18:52:57 -0600

    added environment variable to mongoose connect

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ const express = require('express'),
   const Users = Models.User;
   const app = express();
 
-  // mongoose.connect('mongodb://localhost:27017/movieDB', { 
-  // 	useNewUrlParser: true, useUnifiedTopology: true 
-  // });
-  mongoose.connect('mongodb+srv://khurdt:Xz7ve7ybKaPqgAmO@khurdt.jb0ie.mongodb.net/movieDB?retryWrites=true&w=majority', { 
-  	useNewUrlParser: true, useUnifiedTopology: true 
-  });
+  // mongoose.connect('mongodb://localhost:27017/movieDB', {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //------MiddleWare---------------------------------------------
 
