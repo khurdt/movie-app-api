0e35330c54c80567eea800332d5c2db544dfcc3d
Kevin Hurdt <joshuahurdt@gmail.com>
2022-02-21 05:05:45 -0600

    temporarily removing CORS

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ app.use(bodyParser.json());
 //this must come after the middleware bodyParser urlencoded
 
 //setting limits to what domain can access data
-let allowedOrigins = [
-  'https://kh-movie-app.herokuapp.com',
-  'http://127.0.0.1:8080',
-  'http://localhost:1234',
-  'https://m.media-amazon.com'
-];
+// let allowedOrigins = [
+//   'https://kh-movie-app.herokuapp.com',
+//   'http://127.0.0.1:8080',
+//   'http://localhost:1234',
+//   'https://m.media-amazon.com'
+// ];
 
 //implementing limits using CORS
 app.use(cors({
