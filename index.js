2a2cfcee3d9de98c3a926f1c2d1a739fdf75fb1d
Kevin Hurdt <joshuahurdt@gmail.com>
2022-02-23 02:25:22 -0600

    updated CORS

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let allowedOrigins = [
   'https://kh-movie-app.herokuapp.com',
   'http://127.0.0.1:8080',
   'http://localhost:1234',
-  'https://www.imdb.com/'
+  'https://www.themoviedb.org/'
 ];
 
 //implementing limits using CORS
